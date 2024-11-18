06d8f9e121c1655c5c21a2d95cf0af5b6b582891
jarnovs <usalmazovich@gmail.com>
2024-11-18 16:16:28 +0600

    Update main.py
    Shorter

--- main.py
+++ main.py
@@ -3,29 +3,22 @@ import random
 location = [4, 5, 6]
 weight = [314, 218, 181]
 
-a = int(input('Enter the first position of the box (km): '))
-b = int(input('Enter the second position of the box (km): '))
-c = int(input('Enter the third position of the box (km): '))
-
-def finder(a,b,c):
+def finder():
+    a = int(input('Enter the first position of the box (km): '))
+    b = int(input('Enter the second position of the box (km): '))
+    c = int(input('Enter the third position of the box (km): '))
     global location
     global weight
     if [a,b,c] != location:
         print('Cargo is not found')
         location = random.sample(range(8), 3)
-        a = int(input('Enter the first position of the box (km): '))
-        b = int(input('Enter the second position of the box (km): '))
-        c = int(input('Enter the third position of the box (km): '))
-        finder(a,b,c)
+        finder()
     else:
         if sum(weight) == 713:
             print('Cargo is found')
         else:
             print("Cargo does not have neccessary weight")
             location = random.sample(range(8), 3)
-            a = int(input('Enter the first position of the box (km): '))
-            b = int(input('Enter the second position of the box (km): '))
-            c = int(input('Enter the third position of the box (km): '))
-            finder(a,b,c)
+            finder()
 
-finder(a,b,c)
+finder()
