e645ce9f35507240c42973ebe0a9a1aee66bfaed
Sanjar <usalmazovich@gmail.com>
2024-11-24 15:00:12 +0600

    Update 3.0
    Divided big function finder() into two smaller: for checking weight, and checking location

--- main.py
+++ main.py
@@ -3,22 +3,28 @@ import random
 location = [4, 5, 6]
 weight = [314, 218, 181]
 
+def check_weight():
+    global weight
+    global location
+
+    if sum(weight) == 713:
+        print('Cargo is found')
+    else:
+        print("Cargo does not have neccessary weight")
+        location = random.sample(range(8), 3)
+        finder()
+
 def finder():
     a = int(input('Enter the first position of the box (km): '))
     b = int(input('Enter the second position of the box (km): '))
     c = int(input('Enter the third position of the box (km): '))
+
     global location
-    global weight
     if [a,b,c] != location:
         print('Cargo is not found')
         location = random.sample(range(8), 3)
         finder()
     else:
-        if sum(weight) == 713:
-            print('Cargo is found')
-        else:
-            print("Cargo does not have neccessary weight")
-            location = random.sample(range(8), 3)
-            finder()
+        check_weight():
 
 finder()
