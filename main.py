eb99de947f10114538fa22d25892533f711da574
Sanjar <usalmazovich@gmail.com>
2024-11-24 15:15:26 +0600

    Adding Comments

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 location = [4, 5, 6]
 weight = [314, 218, 181]
 
+# Function for cheking weight
 def check_weight():
     global weight
     global location
@@ -11,9 +12,12 @@ def check_weight():
         print('Cargo is found')
     else:
         print("Cargo does not have neccessary weight")
+        # Location Randomizer
         location = random.sample(range(8), 3)
         finder()
 
+
+# Function for entering and checking coordinates
 def finder():
     a = int(input('Enter the first position of the box (km): '))
     b = int(input('Enter the second position of the box (km): '))
@@ -22,9 +26,10 @@ def finder():
     global location
     if [a,b,c] != location:
         print('Cargo is not found')
+        # Location Randomizer
         location = random.sample(range(8), 3)
         finder()
     else:
-        check_weight():
+        check_weight()
 
 finder()
